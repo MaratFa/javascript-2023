8e9bd4b440a2cca7403e3b6fdfa9a369ceb17145
Marat <evolution99@rambler.ru>
2023-07-09 15:48:28 +0300

    First commit

---
+++ 01-base/app.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(num, num2)
+
+// let num3 = (num + 10 * 2) / (5 - 1)
+// console.log(num3)
+
+// const fullName = firstName + "Fa"
+// const fullName = firstName + "' ' " + 'Fa'
+
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const input2 = document.getElement('input2')
+
+// console.log(resultElement.textContent)
+// resultElement.textContent = (42 - 2) / num
+
+const sum = input1.value + input2.value
+resultElement.textContent = sum
+console.log(typeof sum)
